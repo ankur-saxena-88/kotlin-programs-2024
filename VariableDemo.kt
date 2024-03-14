5d5d06e2197dae3f3333593bfe84f7c37b4f563f
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-03-14 11:44:11 +0530

    Add "VariableDemo.kt" file

---
+++ VariableDemo.kt
@@ -0,0 +1,18 @@
+// VariableDemo.kt
+// Date: Thu, 14-03-2024
+
+fun main(){
+
+    // val for constant value (value cannot be changed)
+    val name = "Alice" // this is string variable
+
+    // var for mutable value (value can change)
+    var age = 30 // this is number type or integer type variable
+    // printing age
+    println("Age: $age")
+    // changing age value
+    age = 35
+
+    // printing values
+    println("Name: $name\nAge: $age")
+}
