3a728ba17d197cb381beadc8fadc782552c965da
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-03-14 11:27:23 +0530

    Add "HelloKotlin.kt" file

---
+++ HelloKotlin.kt
@@ -0,0 +1,10 @@
+// HelloKotlin.kt
+// Date: Wed, 13-03-2024
+
+// program start
+fun main (args: Array<String>){
+
+    // printing "Hi, Kotlin World!"
+    println ("Hi, Kotlin World!")
+}
+// program end
