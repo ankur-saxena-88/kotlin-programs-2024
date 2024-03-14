557dcec827e9d8170ed35ab6789a965f1072c8ff
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-03-14 11:28:27 +0530

    Add "HelloKotlin1.kt" file

---
+++ HelloKotlin1.kt
@@ -0,0 +1,17 @@
+// HelloKotlin1.kt
+// Date: Wed, 13-03-2024
+
+/*
+Write a Kotlin program to print the following message on the console screen using single print() function.
+
+Hi, Ankur Saxena!
+Welcome to the world of Kotlin.
+*/
+
+// program start
+fun main(){
+
+    // printing message using "\n" newline escape sequence
+    println("Hi, Ankur Saxena!\nWelcome to the world of Kotlin.")
+}
+// program end
