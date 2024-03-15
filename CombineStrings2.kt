2317123e0d63a6e0404426dfb2807783e7ecb300
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-03-15 11:09:46 +0530

    Add "CombineStrings2.kt" file
    This program combines two strings using the String Templates method.

---
+++ CombineStrings2.kt
@@ -0,0 +1,16 @@
+// CombineStrings2.kt
+// Date: Fri, 15-03-2024
+
+// Using String Templates
+/*
+~ String Templates provide a more readable way to combine strings.
+~ String templates are enclosed in dollar sign ($) and can include variable names or expressions within curly braces {}.
+*/
+fun main(){
+
+    val name = "Ankur Saxena"
+    val greeting = "Hi, ${name}!"
+
+    // print greeting
+    println("$greeting")
+}
