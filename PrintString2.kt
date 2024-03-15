84a0fddfffb09b7502b8591db15ec2df82acbac7
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-03-15 11:38:41 +0530

    Add PrintString2.kt file
    Assignment 6

---
+++ PrintString2.kt
@@ -0,0 +1,27 @@
+// PrintString2.kt
+// Date: Fri, 15-03-2024
+
+/*
+This program display the following strings on the console screen using the String templates method ~
+
+Hi, Ankur Saxena!
+You are now 35 years old.
+
+name = "Ankur Saxena",
+age = 35
+*/
+
+fun main(){
+
+    val name = "Ankur Saxena"
+    val age = 35
+
+    println("Hi, ${name}!\nYou are now ${age} years old.")
+}
+
+/*
+Out:
+
+Hi, Ankur Saxena!
+You are now 35 years old.
+*/
