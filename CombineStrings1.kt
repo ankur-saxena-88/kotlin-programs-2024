b441fb380f50d1867f4f6dcc7b11162eca7b324d
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-03-15 10:55:59 +0530

    Add "CombineStrings1.kt" file
    This program combines two strings using the Plus Operator (+).

---
+++ CombineStrings1.kt
@@ -0,0 +1,14 @@
+// CombineStrings1.kt
+// Date: Fri, 15-03-2024
+
+fun main(){
+
+    val firstName = "Ankur"
+    val lastName = "Saxena"
+
+    // combining strings
+    val fullName = firstName + " " + lastName
+
+    // print result
+    println("$fullName")
+}
