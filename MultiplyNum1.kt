7df02db25803db09d71fa018f7a56fe43f80c141
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-03-18 14:02:56 +0530

    Add MultiplyNum1.kt file
    Test data: 12 * 15
    Expected output: 180

---
+++ MultiplyNum1.kt
@@ -0,0 +1,12 @@
+// MultiplyNum1.kt
+// Date: Thu, 14-03-2024
+// Kotlin program to find the multiplication of two numbers.
+// Where, num1 = 12 and num2 = 15
+
+fun main(){
+
+    // calculate the multiplication
+    val result = 12 * 15
+    // printing result
+    println(result)
+}
