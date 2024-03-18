97d06678fbb5f279e165832994e11a0c836dfea1
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-03-18 17:36:35 +0530

    Add FindMultiply1.kt file

---
+++ FindMultiply1.kt
@@ -0,0 +1,23 @@
+// FindMultiply1.kt
+// Date: Sat, 16-03-2024
+
+/*
+Write a kotlin program to find the multiplication of two numbers.
+Where, num1 = 100 and num2 = 200
+*/
+
+fun main(){
+
+    val num1 = 100
+    val num2 = 200
+
+    // printing value
+    println("First number is: $num1")
+    println("Second number is: $num2")
+
+    // calculate multiplication
+    val result = num1 * num2
+
+    // printing result
+    println("The multiplication of $num1 and $num2 is :: $result")
+}
