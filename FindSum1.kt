18cfbb5d82ec51942025e8d00fd7fc1b51203000
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-03-18 14:17:11 +0530

    Add FindSum1.kt file

---
+++ FindSum1.kt
@@ -0,0 +1,22 @@
+// FindSum1.kt
+// Date: Fri, 15-03-2024
+
+/*
+Write a Kotlin program to find the addition of two numbers.
+Where, num1 = 45 and num2 = 50
+*/
+
+fun main(){
+
+    val num1 = 45
+    val num2 = 50
+
+    // calculate sum
+    val findSum = num1 + num2
+
+    println("First number is: $num1")
+    println("Second number is: $num2")
+
+    // printing result
+    println("The sum of $num1 and $num2 is :: $findSum")
+}
