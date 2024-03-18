f7587c8c563d5e7e57cc7d05e6298bfae2f396a3
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-03-18 13:56:33 +0530

    Add Add2.kt file

---
+++ Add2.kt
@@ -0,0 +1,15 @@
+// Add2.kt
+// Date: Wed, 13-03-2024
+// Kotlin program to find the addition of two numbers.
+// Where, num1 = 112 and num2 = 60
+
+fun main(){
+
+    val num1 = 112
+    val num2 = 60
+
+    val sum = num1 + num2
+
+    // printing sum
+    println("The sum of $num1 and $num2 is :: $sum")
+}
