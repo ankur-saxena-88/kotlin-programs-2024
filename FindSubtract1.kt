98c6d73d7d5571f4f309398936947a0ad6050b08
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-03-18 17:14:51 +0530

    Add FindSubtract1.kt file

---
+++ FindSubtract1.kt
@@ -0,0 +1,23 @@
+// FindSubtract1.kt
+// Date: Fri, 15-03-2024
+
+/*
+Write a Kotlin program to find the subtraction of two numbers.
+Where, num1 = 50 and num2 = 15
+*/
+
+fun main(){
+
+    val num1 = 50
+    val num2 = 15
+
+    // printing value
+    println("First number is: $num1")
+    println("Second number is: $num2")
+
+    // calculate subtraction
+    val result = num1 - num2
+
+    // printing result
+    println("The subtraction of $num1 and $num2 is :: $result")
+}
