46e37809701ba80822d1debe8eaf400e6a9fc858
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-03-18 14:08:32 +0530

    Add FindDivision1.kt file
    Test data: 50/3
    Expected output: 16

---
+++ FindDivision1.kt
@@ -0,0 +1,12 @@
+// FindDivision1.kt
+// Date: Thu, 14-03-2024
+// Kotlin program to find the division of two numbers.
+// Where, num1 = 50 and num2 = 3
+
+fun main(){
+
+    // calculate the division
+    val division=50/3
+    // printing the result
+    println("$division")
+}
