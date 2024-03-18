9205a4877e4bcd2e96338babeeabcbe531801c06
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-03-18 13:51:11 +0530

    Add Add1.kt file

---
+++ Add1.kt
@@ -0,0 +1,11 @@
+// Add1.kt
+// Date: Wed, 13-03-2024
+
+// Kotlin program to find the sum of two numbers.
+// Where, num1 = 74 and num2 = 36
+
+fun main(){
+
+    val sum = 74 + 36
+    println("$sum")
+}
