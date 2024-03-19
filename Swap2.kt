db4d93766ef54f02c34c1f2c705ac383220ec593
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-03-19 11:11:28 +0530

    Add Swap2.kt file

---
+++ Swap2.kt
@@ -0,0 +1,26 @@
+// Swap2.kt
+// Date: Tue, 19-03-2024
+
+/*
+Write a Kotlin program to swap two numbers using + and - operators.
+Where, num1 = 10 and num2 = 20
+*/
+
+fun main(){
+
+    var num1 = 10
+    var num2 = 20
+
+    println("Before swapping....")
+    println("First number is: $num1")
+    println("Second number is: $num2")
+
+    // perform swapping
+    num1 = num1 + num2 // num1 = 10 + 20 = 30
+    num2 = num1 - num2 // num2 = 30 - 20 = 10
+    num1 = num1 - num2 // num1 = 30 - 10 = 20
+
+    println("After swapping....")
+    println("First number is: $num1")
+    println("Second number is: $num2")
+}
