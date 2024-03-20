abd18516243defe293dabbb3e50e232ad17ef7cb
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-03-20 10:05:43 +0530

    Add MultiplicationTable1.kt file

---
+++ MultiplicationTable1.kt
@@ -0,0 +1,26 @@
+// multiplicationtable1.kt
+// Date: Wed, 20-03-2024
+
+/*
+Write a Kotlin program to print the multiplication table of 15 on the console screen.
+*/
+
+fun main(){
+
+	val num1 = 15
+
+	println("The multiplication table of $num1 is give below....")
+
+	println("$num1 x 1 = ${num1 * 1}")
+	println("$num1 x 2 = ${num1 * 2}")
+	println("$num1 x 3 = ${num1 * 3}")
+	println("$num1 x 4 = ${num1 * 4}")
+	println("$num1 x 5 = ${num1 * 5}")
+	println("$num1 x 6 = ${num1 * 6}")
+	println("$num1 x 7 = ${num1 * 7}")
+	println("$num1 x 8 = ${num1 * 8}")
+	println("$num1 x 9 = ${num1 * 9}")
+	println("$num1 x 10 = ${num1 * 10}")
+	println("$num1 x 11 = ${num1 * 11}")
+	println("$num1 x 12 = ${num1 * 12}")
+}
