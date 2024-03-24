782ee184301367d31260497123357cba198be36a
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-03-24 17:03:09 +0530

    Add DemoUnaryOpp.kt file

---
+++ DemoUnaryOpp.kt
@@ -0,0 +1,17 @@
+// DemoUnaryOpp.kt
+// Date: Sun, 24-03-2024
+
+/*
+Kotlin - Unary operator demo program
+Where, num = 10
+*/
+
+fun main(){
+
+    var num = 10
+
+    println("First print then increment: ${num++}")
+    println("First increment then print: ${++num}")
+    println("First print then decrement: ${num--}")
+    println("First decrement then print: ${--num}")
+}
