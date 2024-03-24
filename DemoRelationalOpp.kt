c5047e3b3cecd7cd7a21867c9bc5fdaef39098b3
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-03-24 15:12:21 +0530

    Add DemoRelationalOpp.kt file

---
+++ DemoRelationalOpp.kt
@@ -0,0 +1,25 @@
+// DemoRelationalOpp.kt
+// Date: Sun, 24-03-2024
+
+/*
+Kotlin - Relational operator demo program
+Where,
+~ Value of the first variable is : 30
+~ Value of the second variable is : 25
+*/
+
+fun main() {
+    
+    val a = 30
+    val b = 25
+    
+    // print values
+    println("a = $a, b = $b")
+    
+    println("a > b : ${a > b}")   // Greater than
+    println("a < b : ${a < b}")   // Less than
+    println("a >= b : ${a >= b}") // Greater than or equal to
+    println("a <= b : ${a <= b}") // Less than or equal to
+    println("a == b : ${a == b}") // Equal to
+    println("a != b : ${a != b}") // Not equal to
+}
