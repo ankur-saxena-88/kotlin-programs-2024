ca6eee7ad7969ea5306ba6705e0f9858f3152bb5
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-03-24 15:02:17 +0530

    Add DemoArithmeticOpp.kt file

---
+++ DemoArithmeticOpp.kt
@@ -0,0 +1,19 @@
+// DemoArithmeticOpp.kt
+// Date: Sun, 24-03-2024
+
+/*
+Kotlin - Arithmetic operators demo program
+where, first var = 20 and second var = 4
+*/
+
+fun main(){
+    
+    var a = 20 
+    var b = 4 
+    
+    println("a + b = ${(a + b)}")
+    println("a - b = ${(a - b)}")
+    println("a * b = ${(a * b)}")
+    println("a / b = ${(a / b)}")
+    println("a % b = ${(a % b)}")
+}
