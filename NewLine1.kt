3f34c5ac560eb08dd763cfd8c89fb6524d2e1cde
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-03-24 12:17:48 +0530

    Add NewLine1.kt file

---
+++ NewLine1.kt
@@ -0,0 +1,17 @@
+// NewLine1.kt
+// Date: Sun, 24-03-2024
+
+/*
+Kotlin - The New Line Program
+
+Expected Output:
+
+Welcome
+to the world
+of Kotlin!
+*/
+
+fun main(){
+
+    println("Welcome\nto the world\nof Kotlin!")
+}
