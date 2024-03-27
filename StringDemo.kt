3fc8247afd09a5db5059888f3bb32025789e3205
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-03-27 14:28:02 +0530

    Add StringDemo.kt file

---
+++ StringDemo.kt
@@ -0,0 +1,13 @@
+// StringDemo.kt
+// Date: Wed, 27-03-2024
+
+fun main(){
+
+    var name: String
+    name = "John"
+    println(name)
+
+    print("Hello, ")
+    print("world!") // Output: Hello, world!
+    println("Welcome to Kotlin!") // Output: Welcome to Kotlin!
+}
