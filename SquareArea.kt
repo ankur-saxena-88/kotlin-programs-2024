a10a7ec3caae3f6206e2523274c9e8d9a59b6d73
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-03-28 14:05:53 +0530

    Add SquareArea.kt file

---
+++ SquareArea.kt
@@ -0,0 +1,22 @@
+// SquareArea.kt
+// Date: Thu, 28-03-2024
+
+/*
+Write a Kotlin program to calculate the area of a square.
+Where, value of the side = 12
+
+Area of a square formula = side * side
+*/
+
+fun main(){
+
+    val side = 12.0
+
+    println("Value of the side: $side")
+
+    // calculate area
+    val area = side * side
+
+    // print result
+    println("Area of a square:: $area")
+}
