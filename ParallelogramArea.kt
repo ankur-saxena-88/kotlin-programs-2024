88778eaed487733d763a3287190685f9b6fcbfde
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-04-01 10:15:38 +0530

    Add ParallelogramArea.kt file

---
+++ ParallelogramArea.kt
@@ -0,0 +1,25 @@
+// ParallelogramArea.kt
+// Date: Mon, 01-04-2024
+
+/*
+Write a Kotlin program to calculate the area of a parallelogram.
+Where, base = 10 and height = 8
+
+Formula: Area of a parallelogram = (base * height)
+*/
+
+fun main(){
+
+    var base = 10
+    var height = 8
+
+    // print values
+    println("Value of base: $base")
+    println("Value of height: $height")
+
+    // calculate area
+    var area = (base * height)
+
+    // print result
+    println("Area of a parallelogram:: $area")
+}
