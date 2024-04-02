4573f7a97c7a073c4c19db8b8a3e2f421095cd4e
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-04-02 10:25:32 +0530

    Add DisplayStringLength.kt file

---
+++ DisplayStringLength.kt
@@ -0,0 +1,21 @@
+// DisplayStringLength.kt
+// Date: Tue, 02-04-2024
+
+/*
+Write a Kotlin program to print the length of a string.
+Where, string = "Hi, welcome to the world of Kotlin!"
+*/
+
+fun main() {
+
+    val string = "Hi, welcome to the world of Kotlin!"
+
+    // print string
+    println("The string is: $string")
+
+    // find string length
+    val stringLength = string.length
+
+    // print result
+    println("The length of the string is: $stringLength")
+}
