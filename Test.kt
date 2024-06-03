c12e313258cce6683ce1248b84e223c27d385f6b
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-06-03 11:24:18 +0530

    Add Test.kt file

---
+++ Test.kt
@@ -0,0 +1,12 @@
+fun Test1(){
+
+    println("Hi, Kotlin World!")
+}
+
+fun main(){
+
+    // calling function
+    //Test1() // method 1
+    //println("${Test1()}") // method 2
+    println(Test1()) // method 3
+}
