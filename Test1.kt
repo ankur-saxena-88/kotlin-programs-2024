0a96080a4cf789a3d498ac94b845ff0a9f4692ac
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-06-03 11:24:51 +0530

    Add Test1.kt file

---
+++ Test1.kt
@@ -0,0 +1,16 @@
+fun test1(){
+
+    val name: String = "Rover"
+    val age: Int = 12
+
+    println("Hi, $name!")
+    println("You are now $age years old!")
+    println("Have a good day....")
+}
+
+// creating main function
+fun main(){
+
+    // calling function
+    println("${test1()}")
+}
