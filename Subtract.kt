503ad8ac444f52acdd954c62e255cc86859b2db8
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-06-08 20:39:33 +0530

    Add Subtract.kt file

---
+++ Subtract.kt
@@ -0,0 +1,26 @@
+// Subtract.kt
+// Tue, 28-05-2024
+
+/*
+Kotlin program to find the subtraction of two numbers.
+Where, num1 = 65 & num2 = 16
+ */
+
+fun findSubtraction(){
+    val num1 = 65
+    val num2 = 16
+
+    println("First number is: $num1")
+    println("Second number is: $num2")
+
+    // calculate subtraction
+    val subtract = num1 - num2
+
+    // print result
+    println("The subtraction of $num1 and $num2 is :: $subtract")
+}
+
+fun main(){
+    // calling function
+    findSubtraction()
+}
