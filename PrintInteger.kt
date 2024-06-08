bdc569b931fe8253fcdf6de2966c6f1a8b4cd46a
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-06-08 21:04:01 +0530

    Add PrintInteger.kt file

---
+++ PrintInteger.kt
@@ -0,0 +1,12 @@
+// PrintInteger.kt
+// Sat, 08-06-2024
+
+fun printInteger(){
+    val num = 15
+    println("Integer value = $num")
+}
+
+fun main(){
+    // calling function
+    printInteger()
+}
